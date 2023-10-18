639b179220fd0bdf368948e07fd3033717d70580
Paul David Tan <cryxuz@gmail.com>
2023-10-18 13:44:56 +1300

    swapi api -starwars

---
+++ app.js
@@ -0,0 +1,13 @@
+// starwars api
+
+// const getStarWarsPerson = async (id) => {
+//   try {
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}`)
+//     console.log(res.data)
+//   } catch (e) {
+//     console.log("Error! :", e)
+//   }
+// }
+// 
+// getStarWarsPerson(1)
+
