74a77140ec2c43c677d9e5f3de9e013ac97cd973
Paul David Tan <cryxuz@gmail.com>
2023-10-18 13:45:32 +1300

    dad jokes api

--- app.js
+++ app.js
@@ -11,3 +11,31 @@
 // 
 // getStarWarsPerson(1)
 
+// dad jokes api render in homepage.
+
+const jokes = document.querySelector('#jokes')
+const button = document.querySelector('button')
+
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke()
+  console.log(jokeText)
+  const newLI = document.createElement('LI')
+  newLI.append(jokeText)
+  jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
+  try {
+    const config = {headers: {Accept: 'application/json'}}
+    const res = await axios.get('https://icanhazdadjoke.com/', config)
+    // console.log(res.data.joke)
+    return res.data.joke
+  } catch (e) {
+    return "NO JOKES AVAILABLE! SORRY"
+  }
+  
+}
+// call the function in the console
+// getDadJoke()
+button.addEventListener('click', addNewJoke)
